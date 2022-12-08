145594bc46e1b4824f4cfb0edf4629f235b9656c
eogarvin <egarvin@student.ethz.ch>
2022-12-08 17:54:09 +0100

    .

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Pynpoint_ifs',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Emily O. Garvin',
+    author_email='egarvin@phys.ethz.ch',
+    description='Package for IFS cube reduction'
+)
