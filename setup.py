2aa152d75eb10259fb0f058cb2c02c0aac7902c3
eogarvin <egarvin@student.ethz.ch>
2023-01-04 13:19:52 +0100

    Adding background files

--- setup.py
+++ setup.py
@@ -3,12 +3,24 @@ from distutils.core import setup
 setup(
     name='Pynpoint_ifs',
     version='0.1',
-    packages=[''],
+    packages=['Pynpoint_ifs'],
     url='',
     license='',
     author='Emily O. Garvin',
     author_email='egarvin@phys.ethz.ch',
-    description='Package for IFS cube reduction'
+    description='Package for IFS cube reduction',
+    install_requires=[
+                'numpy',
+                'scipy',
+                'matplotlib',
+                'astropy',
+                'PyAstronomy',
+                'pandas',
+                'sklearn',
+                'seaborn',
+                'spectres',
+                'petitRADTRANS',
+                'photutils'],
 )
 
 # Dependencies: Pynpoint, petitRADTRANS?, spectres?
