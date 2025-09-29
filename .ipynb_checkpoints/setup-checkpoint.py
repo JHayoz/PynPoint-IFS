eef15b28a6f03a6861cc5ad1948f04bf8a75b156
JHayoz <jeanhayoz94@gmail.com>
2025-09-29 16:24:51 +0200

    Added all

---
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='pynpoint-ifs',
+    version='0.1.0',
+    packages=['pynpoint-ifs'],
+    url='',
+    license='',
+    author='Emily O. Garvin',
+    author_email='egarvin@phys.ethz.ch',
+    description='Package for IFS cube reduction',
+    install_requires=[
+                'numpy',
+                'scipy',
+                'matplotlib',
+                'astropy',
+                'PyAstronomy',
+                'pandas',
+                'sklearn',
+                'seaborn',
+                'spectres',
+                'photutils',
+                'skycalc_ipy'],
+)
